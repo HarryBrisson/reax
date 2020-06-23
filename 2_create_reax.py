21468ad11a04999280b71abc63ab8581cc6bf227
Harry Brisson <harry.brisson@nielsen.com>
2020-06-22 22:02:44 -0700

    add more specifications for categories

--- 2_create_reax.py
+++ 2_create_reax.py
@@ -5,11 +5,37 @@ emojis = os.listdir('emojis')
 print(emojis)
 
 def group_emojis(emojis):
+
 	categories = {}
+
 	for e in emojis:
-		category = emoji.replace('man','person').replace('woman','person')\
-				.replace('boy','child').replace('girl','child')\
-				.replace('-type-1-2','').replace('-type-3','')\
-				.replace('-type-4','').replace('type-5','').replace('type-6','')\
-				.replace('medium-light-','').replace('medium-dark-','')\
-				.replace('dark-','').replace('light-','').replace('medium-','')\
+
+		if 'skin-tone' in e or 'emoji-modifier' in e or 'type' in e:
+			print(e)
+
+			category = e.replace('woman','person').replace('man','person')\
+					.replace('female','').replace('male','')\
+					.replace('boy','child').replace('girl','child')\
+					.replace('women','people').replace('men','people')\
+					.replace('-type-1-2','').replace('-type-3','')\
+					.replace('-type-4','').replace('-type-5','').replace('-type-6','')\
+					.replace('medium-light-','').replace('medium-dark-','')\
+					.replace('dark-','').replace('light-','').replace('medium-','')\
+					.replace('haired','hair')\
+					.replace('red-hair','').replace('blond-hair','')\
+					.replace('blonde','').replace('blond','')\
+					.replace('black-hair','').replace('white-hair','')\
+					.replace('dancing','dancer').replace('facepalming','facepalm')\
+					.replace('biking','bicyclist')\
+					.replace('person','').replace('child','')\
+					.replace('-','').replace('skintone','')\
+					.split('_')[0]
+
+			category += "_all"
+
+			if category in categories.keys():
+				categories[category].append(e)
+			else:
+				categories[category] = [e]
+
+	return categories
